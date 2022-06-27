88009a85ac67dd7a5d6c84ee863a99706a4071eb
Raghuitcinfo4 <106382232+Raghuitcinfo4@users.noreply.github.com>
2022-06-27 13:40:20 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,20 +1,32 @@
-package test;
-import org.openqa.selenium.*;
+package selenium;
+import org.openqa.selenium.*; 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class demo {
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String path1="D:\\Selenium\\chromedriver_win32\\chromedriver.exe";
+public class demo 
+{
+	public static void main(String[] args) 
+	{ 
+		String path1="D:\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", path1);
-		WebDriver driver=new ChromeDriver();
-		driver.get("htts://adactinhotelapp.com/");
-		//driver.close();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://adactinhotelapp.com/");
+		driver.close();
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
